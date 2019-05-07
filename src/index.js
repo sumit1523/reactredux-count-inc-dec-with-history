0256dd4532da7368c8d9f808aba4727116f7054c
sumit kumar <sumit1523@gmail.com>
2019-05-08 00:01:52 +0530

    Redux Middleware

--- src/index.js
+++ src/index.js
@@ -4,10 +4,20 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {Provider} from 'react-redux';
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import reducer from './store/reducer';
 
-const store = createStore(reducer);
+
+const logAction = store => {
+    return next => {
+        return action => {
+            const result = next(action);
+            console.log(`Caught in the middleware ${JSON.stringify(result)}`)
+        }
+    }
+}
+
+const store = createStore(reducer, applyMiddleware(logAction));
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
