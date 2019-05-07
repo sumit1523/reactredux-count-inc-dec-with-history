ce117a01a79b2432ef0cc91f67af7540582e51db
sumit kumar <sumit.u.kumar@capgemini.com>
2019-05-07 13:12:19 +0530

    state reducer

--- src/App.js
+++ src/App.js
@@ -1,36 +1,53 @@
 import React, {Component} from 'react';
 import './App.css';
+import {connect} from 'react-redux';
 
 class App extends Component {
 
-  state = {
-    age:21
-  }
+  // state = {
+  //   age:21
+  // }
 
-  onAgeUp = () => {
-    this.setState({
-      ...this.state,
-      age:++this.state.age
-    })
-  };
+  // onAgeUp = () => {
+  //   this.setState({
+  //     ...this.state,
+  //     age:++this.state.age
+  //   })
+  // };
 
-  onAgeDown = () => {
-    this.setState({
-      ...this.state,
-      age:--this.state.age
-    })
-  };
+  // onAgeDown = () => {
+  //   this.setState({
+  //     ...this.state,
+  //     age:--this.state.age
+  //   })
+  // };
+
+
   render() {
 
     return (
       <div className="App">
-       <div>Age: <span>{this.state.age}</span></div>
-       <button onClick= {this.onAgeUp}>AgeUp</button>
-       <button onClick= {this.onAgeDown}>AgeDown</button>
+       <div>Age: <span>{this.props.age}</span></div>
+       <button onClick= {this.props.onAgeUp}>AgeUp</button>
+       <button onClick= {this.props.onAgeDown}>AgeDown</button>
       </div>
     );
   
 };
 }
 
-export default App;
+const mapStateToPorps = (state) => {
+  return{
+    age: state.age
+  }
+}
+const mapDispachToProps = (dispach) => {
+  return {
+    onAgeUp: () => dispach({type:'AGE_UP'}),
+    onAgeDown: () => dispach({type: 'AGE_DOWN'})
+  }
+}
+
+
+
+export default connect(mapStateToPorps, mapDispachToProps)(App);
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -5,6 +5,13 @@ const initialState = {
 const reducer = (state = initialState, action) => {
    const newState = {...state};
 
+   if(action.type === 'AGE_UP'){
+    newState.age++;
+   }
+   if(action.type === 'AGE_DOWN'){
+    newState.age--;
+   }
+
    return newState;
 };
 
